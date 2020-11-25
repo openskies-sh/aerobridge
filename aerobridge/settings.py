5eb10a2d45cd3c34c3cd16de1bdba92c195e62db
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2020-11-25 09:49:33 +0000

    Move Django secret as a environment variable

--- aerobridge/settings.py
+++ aerobridge/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9br(nycnvtilfdnb@jeo%icp)i3f^@&03#)3&#uugx9!-q%hn#'
+SECRET_KEY = env.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
