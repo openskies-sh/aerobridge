cfbaae8757540fe49ca1dacb38da6ad56df867a7
Hrishikesh Ballal <hballal@gmail.com>
2020-12-07 21:49:00 +0000

    Update urls.py

--- digitalsky_provider/urls.py
+++ digitalsky_provider/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("drone_register/<uuid:pk>", ds_views.AircraftRegisterDetail.as_view(), name="sign_drone"),    
 
     path("register_drone/<uuid:pk>", ds_views.RegisterDrone.as_view(), name="register_drone"),    
+    
     path("all_permissions/", ds_views.FlyDronePermissionApplicationList.as_view(), name="download_artefact"),    
     path("apply_permission/<uuid:pk>", ds_views.FlyDronePermissionApplicationDetail.as_view(), name="apply_permission"),
     path("download_permission_artefact/<uuid:pk>", ds_views.DownloadFlyDronePermissionArtefact.as_view(), name="download_artefact"),  
