5b4340fc51d140626c4f40b5f7890ca42f53cc87
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-04-23 21:26:30 +0100

    Update models.py

--- pki_framework/models.py
+++ pki_framework/models.py
@@ -5,8 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 class DigitalSkyCredentials(models.Model):
     ''' A class to store tokens from Digital Sky '''
     
-    TOKEN_TYPE= ((0, _('DIGITAL SKY OPERATOR')),(1, _('DIGITAL SKY MANUFACTURER')),(2, _('DRONE')),)
+    TOKEN_TYPE= ((0, _('DIGITAL SKY OPERATOR')),(1, _('DIGITAL SKY MANUFACTURER')),(2, _('DIGITAL SKY PILOT')),(3, _('RFM')),)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     token_type = models.IntegerField(choices=TOKEN_TYPE)
     token = models.BinaryField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
