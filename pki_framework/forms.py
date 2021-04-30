381cfa1f9c10094f65b5af5e46bed3c60f595057
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-04-30 22:46:52 +0100

    Update forms.py

--- pki_framework/forms.py
+++ pki_framework/forms.py
@@ -3,7 +3,9 @@ from django.forms import ModelForm, widgets
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-TOKEN_TYPE= ((0, _('DIGITAL SKY OPERATOR')),(1, _('DIGITAL SKY MANUFACTURER')),(2, _('DRONE')),)
+KEY_ENVIRONMENT = ((0, _('DIGITAL SKY OPERATOR')),(1, _('DIGITAL SKY MANUFACTURER')),(2, _('DIGITAL SKY PILOT')),(3, _('RFM')),(4, _('RFM')),)
+
+TOKEN_TYPE= ((0, _('PUBLIC_KEY')),(1, _('PRIVATE_KEY')),(2, _('AUTHENTICATION TOKEN')),(3, _('RFM KEY')),(4, _('OTHER')),)
 
 class TokenCreateForm(ModelForm):
     class Meta:
@@ -12,6 +14,8 @@ class TokenCreateForm(ModelForm):
         
 class CutsomTokenCreateForm(forms.Form):
     name = forms.CharField(max_length=100)
-    token_type = forms.IntegerField(widget=forms.Select(choices=TOKEN_TYPE),
+    token_type = forms.IntegerField(widget=forms.Select(choices=KEY_ENVIRONMENT),
+    )
+    environment = forms.IntegerField(widget=forms.Select(choices=TOKEN_TYPE),
     )
     token = forms.CharField(widget = forms.TextInput())
