bdbae81002a2c6f077dff2050cc6464bf3f14fbd
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-06-18 22:31:05 +0100

    Update serializers.py

--- pki_framework/serializers.py
+++ pki_framework/serializers.py
@@ -12,6 +12,7 @@ class AerobridgeCredentialSerializer(serializers.ModelSerializer):
     
     def get_token(self, digital_sky_credentials):
         token = digital_sky_credentials.token
+        print(type(token))
         secret_key = settings.CRYPTOGRAPHY_SALT.encode('utf-8')
         
         f = encrpytion_util.EncrpytionHelper(secret_key=secret_key)
