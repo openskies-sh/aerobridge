32ca50b1de9548bdc20f3d96ce4ba52e890cdc84
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-07-06 14:01:43 +0100

    Updated to fix S3 bucket

--- launchpad/views.py
+++ launchpad/views.py
@@ -858,16 +858,15 @@ class CloudFilesCreateView(APIView):
                     f.write(chunk)
                 f.flush()
                 
-                s3 = boto3.client('s3', region_name =env.get('S3_REGION_NAME','0'), endpoint_url= endpoint_url, aws_access_key_id=env.get('S3_ACCESS_KEY','0'),aws_secret_access_key=env.get('S3_SECRET_KEY','0'))                
-                
                 try:
+                    s3 = boto3.client('s3', region_name =env.get('S3_REGION_NAME','0'), endpoint_url= endpoint_url, aws_access_key_id=env.get('S3_ACCESS_KEY','0'),aws_secret_access_key=env.get('S3_SECRET_KEY','0'))                               
                     
                     s3.upload_fileobj(f, BUCKET_NAME, os.path.join(file_type, file_name))
                 except NoCredentialsError as ne:                                        
                     context = {'errors':'File not uploaded, problem  with Cloud Bucket credentials'}   
                     return render(request, 'launchpad/cloudfiles_error.html', context)
                 except Exception as e:                     
-                    context = {'errors':'File not uploaded, problem  with Cloud Bucket configuration'}   
+                    context = {'errors':'File not uploaded, problem with Cloud Bucket configuration, it is improperly configured or not supported, please contact your administrator.'}   
                     return render(request, 'launchpad/cloudfiles_error.html', context)
                 else:
                     location = endpoint_url + '/' + file_type + file_name
