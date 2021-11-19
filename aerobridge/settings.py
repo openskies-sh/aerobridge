928d5cd4712797f16ddc5bfa7a44e10e73b8298f
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-11-19 13:44:20 +0000

    Update settings.py

--- aerobridge/settings.py
+++ aerobridge/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
