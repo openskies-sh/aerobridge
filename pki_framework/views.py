391edd4fe3e35e6e8b58609d6354a527f3440fa0
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-11-23 09:02:06 +0000

    Update views.py

--- pki_framework/views.py
+++ pki_framework/views.py
@@ -7,9 +7,8 @@ from django.utils.decorators import method_decorator
 from pki_framework.utils import requires_scopes
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import encrpytion_util
 from rest_framework import status
-from django.conf import settings
+
 # Create your views here.
 
 @method_decorator(requires_scopes(['aerobridge.read', 'aerobridge.write']), name='dispatch')
