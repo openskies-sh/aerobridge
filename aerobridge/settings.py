8fb85d7ce40579c6d01ac5717546aad02148b07a
hrishiballal <hrishikeshballal@yahoo.com>
2023-01-27 15:40:12 +0000

    Added Heroku

--- aerobridge/settings.py
+++ aerobridge/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 import moneyed
@@ -226,3 +226,4 @@ LOGGING = {
     },
 }
 
+django_heroku.settings(locals())
